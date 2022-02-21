d323d5e5e8eab37fd58b06dd5e8d4f1a9e7494d1
pedrocambier <pedrocambier@gmail.com>
2022-02-21 16:38:41 -0300

    added the mouseenter event to draw the pixels and mousedown and up events to only draw when clicked

--- main.js
+++ main.js
@@ -1,8 +1,20 @@
+let pixelCount = 0;
+let mouseDown = false;
+
+window.addEventListener('mousedown', () => mouseDown = true);
+window.addEventListener('mouseup', () => mouseDown = false);
+
+const mouseEnter = (event) => {
+  const div = event.target;
+  if(mouseDown) div.style.backgroundColor = 'black';
+}
+
 function addPixel (size, parentContainer) {
   const pixel = document.createElement('div');
   pixel.style.height = `${size}px`;
   pixel.style.width = `${size}px`;
-  pixel.classList.add('grid-pixel');
+  pixel.classList.add('grid-pixel', `'pixel-${pixelCount++}'`);
+  pixel.addEventListener('mouseenter', mouseEnter);
   parentContainer.appendChild(pixel);
 }
 
@@ -12,7 +24,9 @@ const removeAllChildren = parentContainer => {
     removeAllChildren(parentContainer);
   }
 }
+
 const createGrid = (size, parentContainer) => {
+  pixelCount = 0;
   removeAllChildren(parentContainer);
   const totalWidth = parentContainer.offsetWidth;
   const pixelWidth = totalWidth/size;
@@ -25,3 +39,5 @@ const test = (size) => {
   const div = document.querySelector('div.grid-container');
   createGrid(size, div);
 }
+
+test(16);
